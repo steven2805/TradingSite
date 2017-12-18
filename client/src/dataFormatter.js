9b7b8956d40a399d1e7ddeb1a7b1f0f6c3b4873a
steven2805 <stevenmac90@googlemail.com>
2017-12-18 16:15:31 +0000

    Adding Open data to an array

--- client/src/dataFormatter.js
+++ client/src/dataFormatter.js
@@ -2,11 +2,20 @@
 
 
 function dataFormatter(data){
-    // console.log(Object.values(data)[1])
+
     var timeData = Object.values(data)[1];
     console.log(timeData, "this is the time data output");
     var dayData = days(timeData);
+    var openArray = [];
+    var closeArray = [];
 
+    for(var i = 0; i<Object.values(timeData).length; i ++){
+        var open = openData(Object.values(timeData)[i]);
+        openArray.push(open);
+
+    }
+
+    console.log(openArray, open, 'array information');
 
 }
 
@@ -18,6 +27,9 @@ function days(data){
     return daysArray;
 }
 
+function openData(data){
+    return Object.values(data)[0];
+}
 
 
 
